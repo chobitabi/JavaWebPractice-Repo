b60caf27914e6a6637e12ea7138c9dac87751f4c
chibitabi <erishimada218@gmail.com>
2025-02-18 17:21:12 +0900

    ログインの完了サーブレット

---
+++ src/main/java/controller2_cookieSession/WelcomeServlet.java
@@ -0,0 +1,35 @@
+package controller2_cookieSession;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//セッションにログイン済みであるというデータがあるか確認
+		 HttpSession session = request.getSession(false); 
+		 if (session == null || session.getAttribute("userName")== null) {
+			 response.sendRedirect("login");
+		 } else {
+		 request.setAttribute("userName", session.getAttribute("userName"));
+		 request.getRequestDispatcher("/WEB-INF/view/welcome.jsp")
+			.forward(request, response);
+	}
+	}
+
+}
