8861ede85a951391ae6f05af42b5d6beb7f0bc1e
chibitabi <erishimada218@gmail.com>
2025-03-03 18:42:15 +0900

    会員管理システムの会員登録機能にバリデーション処理を実装

--- src/main/java/controller_dbPractice09/AddMemberServlet.java
+++ src/main/java/controller_dbPractice09/AddMemberServlet.java
@@ -34,25 +34,58 @@ public class AddMemberServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		 //フォームのデータを取得
+		//バリデーション用のフラグ
+		boolean isError = false;
+
+		//フォームのデータを取得とバリデーション
 		String name = request.getParameter("name");
-		Integer age = Integer.parseInt(request.getParameter("age"));
+		request.setAttribute("name", name); //再表示用
+		if (name.isEmpty()) {
+			//エラーメッセージの作成
+			request.setAttribute("nameError", "名前が未入力です。");
+			isError = true; //入力に不備ありと判定
+		}
+
+		//年齢は文字列として取得。未入力でなければ整数へ変換
+		String strAge = request.getParameter("age");
+		Integer age = null;
+		request.setAttribute("age", strAge); //再表示用
+		if (!strAge.isEmpty()) {
+			try {
+				age = Integer.parseInt(strAge);
+			} catch (NumberFormatException e) {
+				//整数に変換できない場合の処理
+				request.setAttribute("ageError", "年齢は整数で入力してください。");
+				isError = true;
+			}
+		}
+
+		// 会員種別、住所はバリデーション不要
 		Integer typeId = Integer.parseInt(request.getParameter("typeId"));
 		String address = request.getParameter("address");
+		request.setAttribute("typeId", typeId); //再表示用
+		request.setAttribute("address", address); //再表示用 
 
-		//取得したデータを箱（Memberオブジェクト)に入れる
+		//入力に不備がある場合は、フォームを再表示し、処理を中断
+		if (isError == true) {
+			request.getRequestDispatcher("/WEB-INF/view/addMember.jsp")
+					.forward(request, response);
+			return;
+		}
+
+		//入力に不備がなければ、取得したデータを箱（Memberオブジェクト)に入れる
 		Member member = new Member();
 		member.setName(name);
 		member.setAge(age);
 		member.setTypeId(typeId);
 		member.setAddress(address);
-		
+
 		try {
-			//DAOを使ってデータをDB二保存
+			//DAOを使ってデータをDBに保存
 			MemberDao memberDao = DaoFactory.createMemberDao();
 			memberDao.insert(member);
 			request.getRequestDispatcher("/WEB-INF/view/addMemberDone.jsp")
-				.forward(request, response);
+					.forward(request, response);
 		} catch (Exception e) {
 			throw new ServletException(e);
 		}
--- src/main/webapp/WEB-INF/view/addMember.jsp
+++ src/main/webapp/WEB-INF/view/addMember.jsp
@@ -13,24 +13,32 @@
 		<table border="1">
 			<tr>
 				<th>名前</th>
-				<td><input type="text" name="name" /></td>
+				<td><c:if test="${not empty nameError}">
+						<p>
+							<c:out value="※${nameError}" />
+						</p>
+					</c:if> <input type="text" name="name" value="<c:out value="${name}" />">
+				</td>
 			</tr>
 			<tr>
 				<th>年齢</th>
-				<td><input type="text" name="age" /></td>
+				<td><c:if test="${not empty ageError}">
+						<p>
+							<c:out value="※${ageError}" />
+						</p>
+					</c:if> <input type="text" name="age" value="<c:out value="${age}" />"></td>
 			</tr>
 			<tr>
 				<th>住所</th>
-				<td><input type="text" name="address" /></td>
+				<td><input type="text" name="address"
+					value="<c:out value="${address}" />"></td>
 			</tr>
 			<tr>
 				<th>会員種別</th>
-				<td>
-					<select name="typeId">
-						<option value="1">通常会員</option>
-						<option value="2">プレミアム会員</option>
-					</select>
-				</td>
+				<td><select name="typeId">
+						<option value="1" <c:if test="${typeId == 1}">selected</c:if>>通常会員</option>
+						<option value="2" <c:if test="${typeId == 2}">selected</c:if>>プレミアム会員</option>
+				</select></td>
 			</tr>
 		</table>
 		<p>
